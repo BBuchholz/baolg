99e2a131e9a3068d99c95470f653b42ded9f16bd
Brent Buchholz <brentbuchholz@gmail.com>
2023-08-19 13:34:12 -0500

    Add BAOLG script from BALG

---
+++ src/pages/about.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <p>---</p>
+    <h1>Beneficent Ambiance Ov Learning Guided</h1>
+    <h5>
+      Ideologically Organized & Dedicated To
+      Deliverables As...
+    </h5>
+    <h1>Bountiful Activizations Ov Legitmate Growings</h1>
+    <h5>Always Improves, Knowing...</h5>
+    <h1>Betterment Awakened Ov Liberations Greater</h1>
+    <h5>
+      Combining Intentions Establishing A...
+    </h5>
+    <h1>Builders Association Ov Layered Generalists</h1>
+  </div>
+</template>
+
+<style scoped>
+.hello {
+  background-color: #0d022f;
+}
+h3 {
+  margin: 40px;
+}
+h1 {
+  margin: 40px;
+  color: #a915a1;
+}
+h5 {
+  margin: 40px;
+  color: #10598a;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
