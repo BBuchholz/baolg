6921978c0d505dd85aee5b2ec7f2a3f682d9a280
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-17 06:42:15 -0500

    Add SYW5

---
+++ src/pages/myrKiSs/SYW5.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1 class="display">
+    "SYW5"
+  </h1>
+
+  <p class="display">
+    What you see here is a work in progress, this is free flowing, straight off the top idea spewing that will be shaped over time. The emphasis within tha Myriad EcoSystem Arrangement is upon creating and shaping and non-judgemental sharing of process in progress.
+  </p>
+
+  <hr>
+
+  <p class="display">
+    As a creative type the people you want to work with your collaborative acquaintances if you will are the people that are doing things that you find exciting and invigorating these are the sorts of things I would want to share anyways and one of the most common problems amongst creatives is getting an audience for what you're creating you obviously enjoy creating it but so often we find it's not until many years later that someone is discovered for what they've spent so many years developing we are all about realizing that we are all in this together your problem is our problem one creative benefits from all creatives sharing together and that is the essence of tha Myriad EcoSystem
+  </p>
+
+  <div class="card">
+    <img src="https://madamadam.s3.us-east-2.amazonaws.com/SYW5-ac89_CARD.png">
+  </div>
+</template>
+
+<style>
+  .display {
+    margin: 40px;
+  }
+  a.highlighted {
+    color: red;
+  }
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
