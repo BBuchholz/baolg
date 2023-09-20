4e9f6d35fd9a3b1aecec031ec556d9b0cd5300ee
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-19 22:27:33 -0500

    Add SESH

---
+++ src/pages/myrKiSs/SESH.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1 class="display">
+    "SYW5"
+  </h1>
+
+  <p class="display">
+    Projeckt currently looking for a home, seeking placement within tha Myriad EcoSytem Arrangement.
+  </p>
+
+  <p class="display">
+    These are session status codes being used internally at BAOLG
+  </p>
+
+  <p class="display">
+    If you are interested in learning more, contact us!
+  </p>
+
+  <hr>
+
+  <table>
+    <tr>
+      <td>Status</td>
+      <td>Meaning</td>
+    </tr>
+    <tr>
+      <td>w.on.e</td>
+      <td>single session, quick work, we on w.on.e</td>
+    </tr>
+    <tr>
+      <td>Tu/o</td>
+      <td>more than one, cleaving? Splitting? Slicing?</td>
+    </tr>
+    <tr>
+      <td>ð&#39;ree</td>
+      <td>ongoing, breathing</td>
+    </tr>
+    <tr>
+      <td>fo(re)</td>
+      <td>collaboration, birthing</td>
+    </tr>
+  </table>
+
+  <hr>
+
+  <!-- <div class="card">
+    <img src="https://madamadam.s3.us-east-2.amazonaws.com/SYW5-ac89_CARD.png">
+  </div> -->
+</template>
+
+<style>
+  .display {
+    margin: 40px;
+  }
+  a.highlighted {
+    color: red;
+  }
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
