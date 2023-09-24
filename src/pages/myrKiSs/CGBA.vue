d9e9cab1b2c92ebb9801e33332f1163a46a912f5
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-24 10:40:49 -0500

    Add dev notes for WorkIng Progress CGBA.CGBA

---
+++ src/pages/myrKiSs/CGBA.vue
@@ -0,0 +1,58 @@
+<template>
+  <p class="display">
+    Dev Notes For WorkIng Progress
+    <a
+      href="https://cgba.netlify.app/myrKiSs/cgba"
+      target="_blank"
+    >
+      CGBA.CGBA
+    </a>
+  </p>
+  <p class="display">
+    like Rant Casey style back and forths with different characters, done "blockquote" style with different fonts for different voices
+  </p>
+  <p class="display">
+    NB: colored borders are just to distinguish personas for now, will be replacing with distinct fonts for each different voice and no borders (should be blockquotes though)
+  </p>
+  <p class="display">
+    Rant as inspiration, Capote Book as legacy to that, etc.
+  </p>
+  <p class="display">
+    REMIX is one ov tha Foundational PrincipalS ov tha Myriad EcoSystem Arrangement (ACRONYMS-ONLY: REMIX is 1 ov tha Frames Per Second ov tha MESA)
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img
+      class="card"
+      src="https://madamadam.s3.us-east-2.amazonaws.com/CGBA-13c9.png"
+    >
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    a {
+      color: red;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
