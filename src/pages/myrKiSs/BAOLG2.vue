fd0645e614c7f2f7300c618061c61f7beeaeb89f
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-24 09:06:31 -0500

    Add page content for BAOLG2

---
+++ src/pages/myrKiSs/BAOLG2.vue
@@ -0,0 +1,36 @@
+<template>
+  <p class="display">
+    WellCome 2 tha BAOLG (Builders Association ov Layered Generalists) This is an instance of a Workshop Portfolio, one of the CENTRAL (pun intended) organizing features in tha Decentralized Myriad EcoStem Arrangment (tha Decentralized MESA)
+  </p>
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img
+      class="card"
+      src="https://madamadam.s3.us-east-2.amazonaws.com/BAOLG-0c83.png"
+    >
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info goes here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+  </style>
