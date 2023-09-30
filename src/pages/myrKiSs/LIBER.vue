83945cc3e3c5f62fe32451d39edadc82d01c52ad
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-30 05:56:14 -0500

    Add LIBER'

---
+++ src/pages/myrKiSs/LIBER.vue
@@ -0,0 +1,86 @@
+<template>
+  <p class="display">
+    Projeckt: Add BooK ReFs...
+  </p>
+
+  <div>
+    <!-- <a href="#" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/LIBER-65dcc_CARD.png">
+    <!-- </a> -->
+  </div>
+  <p class="display">
+    card attribution info here
+  </p>
+</template>
+
+  <style>
+    .display {
+      margin: 40px;
+    }
+
+    .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+    }
+
+  /* ///////////////////////////// BEGIN GOODREADS STYLE CODE */
+
+  .gr_custom_container_1696069565 {
+    /* customize your Goodreads widget container here*/
+    border: 1px solid gray;
+    border-radius:10px;
+    padding: 10px 5px 10px 5px;
+    background-color: #FFFFFF;
+    color: #000000;
+    width: 300px
+  }
+  .gr_custom_header_1696069565 {
+    /* customize your Goodreads header here*/
+    border-bottom: 1px solid gray;
+    width: 100%;
+    margin-bottom: 5px;
+    text-align: center;
+    font-size: 120%
+  }
+  .gr_custom_each_container_1696069565 {
+    /* customize each individual book container here */
+    width: 100%;
+    clear: both;
+    margin-bottom: 10px;
+    overflow: auto;
+    padding-bottom: 4px;
+    border-bottom: 1px solid #aaa;
+  }
+  .gr_custom_book_container_1696069565 {
+    /* customize your book covers here */
+    overflow: hidden;
+    height: 60px;
+      float: left;
+      margin-right: 4px;
+      width: 39px;
+  }
+  .gr_custom_author_1696069565 {
+    /* customize your author names here */
+    font-size: 10px;
+  }
+  .gr_custom_tags_1696069565 {
+    /* customize your tags here */
+    font-size: 10px;
+    color: gray;
+  }
+  .gr_custom_rating_1696069565 {
+    /* customize your rating stars here */
+    float: right;
+  }
+
+/* ////////////////////////////////// END GOODREADS STYLE CODE */
+</style>
